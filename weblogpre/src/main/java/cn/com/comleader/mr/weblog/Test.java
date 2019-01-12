b74d1dd20cbda7b0e9bd2e9ca1a36ca2be709a1c
xkang <xkang8902@163.com>
2019-01-12 21:09:46 +0800

    commit testvcs!

---
+++ weblogpre/src/main/java/cn/com/comleader/mr/weblog/Test.java
@@ -0,0 +1,17 @@
+package cn.com.comleader.mr.weblog;/**
+ * Created by arc on 12/01/2019.
+ */
+
+/**
+ * This is Description
+ *
+ * @author arc
+ * @date 2019/01/12
+ */
+public class Test {
+    public int data;
+    public Test(){}
+    public Test(int num){
+        this.data=num;
+    }
+}
